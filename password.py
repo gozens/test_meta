88e543c3b7fc912eb104d636cb4953f1c6257b9c
test git <testgit@gmail.com>
2023-05-14 19:28:28 +0200

    commit 10

---
+++ password.py
@@ -0,0 +1,17 @@
+
+from repertoire import effter,__ecri_fic__
+from string import ascii_letters
+from random import randint,choices
+from os import walk
+effter()
+ch = r'C:\Users\T_xOx_T\Desktop\git_test'
+ch = walk(ch)
+
+for chemin,rep,fic in ch:break
+# fic = fic[0]
+
+def password():return ''.join(choices(ascii_letters,k=randint(5,9)))
+
+for i in fic:
+    if 'txt' in i:
+        for j in range(1,randint(10,15)):__ecri_fic__(i,password()+'\n','a')
